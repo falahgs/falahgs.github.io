a52a5cd43e5e637bf6d40b5daa8754dbf4005301
Falah Gate Salieh <digital_fgs@yahoo.com>
2019-06-01 02:45:43 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,41 +1,29 @@
-// Daniel Shiffman
-// http://youtube.com/thecodingtrain
-// http://codingtra.in
+// Initialize the Image Classifier method with MobileNet. A callback needs to be passed.
+let classifier;
 
-// Image Classification with ml5
-// https://youtu.be/yNkAuWz5lnY
+// A variable to hold the image we want to classify
+let img;
 
-let mobilenet;
-let puffin;
-
-function modelReady() {
-  console.log('Model is ready!!!');
-  mobilenet.predict(puffin, gotResults);
-}
-
-function gotResults(error, results) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(results);
-    let label = results[0].className;
-    let prob = results[0].probability;
-    fill(0);
-    textSize(64);
-    text(label, 10, height - 100);
-    createP(label);
-    createP(prob);
-  }
-}
-
-function imageReady() {
-  image(puffin, 0, 0, width, height);
+function preload() {
+  classifier = ml5.imageClassifier('MobileNet');
+  img = loadImage('images/bird.png');
 }
 
 function setup() {
-  createCanvas(640, 480);
-  puffin = createImg('https://falahgs.github.io/blob/master/1.jpg', imageReady);
-  puffin.hide();
-  background(0);
-  mobilenet = ml5.imageClassifier('MobileNet', modelReady);
+  createCanvas(400, 400);
+  classifier.classify(img, gotResult);
+  image(img, 0, 0);
+}
+
+// A function to run when we get any errors and the results
+function gotResult(error, results) {
+  // Display error in the console
+  if (error) {
+    console.error(error);
+  } else {
+    // The results are in an array ordered by confidence.
+    console.log(results);
+    createDiv('Label: ' + results[0].label);
+    createDiv('Confidence: ' + nf(results[0].confidence, 0, 2));
+  }
 }
